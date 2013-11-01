c6a6853f6a192151742ae0b90c312e38cb604712
hzwmail <249179710@qq.com>
2013-11-01 08:55:06 +0800

    PocketNews

---
+++ PocketNews/src/main/Model/SuperUser.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Admin
+ * Date: 13-10-31
+ * Time: 下午2:28
+ * To change this template use File | Settings | File Templates.
+ */
+public class SuperUser {
+ public static void main(String[] args){
+     System.out.println("1");
+ }
+}
